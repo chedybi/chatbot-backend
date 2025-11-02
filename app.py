fa7153758e48efc6cdc316353ed80a7fa36e98ff
chedybi <chedy.souilhi@gmail.com>
2025-11-02 17:08:15 +0100

    Add Dockerfile and update Flask backend

--- app.py
+++ app.py
@@ -530,5 +530,8 @@ def handle_exception(e):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
