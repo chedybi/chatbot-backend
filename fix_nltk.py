14786f7ff6c9f26e0452a6af3356c8dc88294a50
chedybi <95747510+chedybi@users.noreply.github.com>
2025-11-05 20:29:45 +0100

    Delete fix_nltk.py

--- fix_nltk.py
+++
@@ -1,9 +0,0 @@
-import nltk
-nltk.data.path.append("C:/Users/Admin/AppData/Roaming/nltk_data")
-
-# Forcer le téléchargement
-nltk.download('punkt', force=True)
-nltk.download('stopwords', force=True)
-nltk.download('wordnet', force=True)
-
-print("Téléchargement terminé avec succès.")
