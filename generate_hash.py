bb9176d1aa9b8e68c0412f304098f73e3a876d65
chedybi <95747510+chedybi@users.noreply.github.com>
2025-11-05 20:51:09 +0100

    Delete generate_hash.py

--- generate_hash.py
+++
@@ -1,6 +0,0 @@
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt()
-password = "Souilhi8"  # ton nouveau mot de passe
-hashed_pw = bcrypt.generate_password_hash(password).decode("utf-8")
-print(hashed_pw)
